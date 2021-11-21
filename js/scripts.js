686ac727501b36ead15dade5aed39ce1a55b450b
Ruweydha Abdinoor <ruweydhaabdinoor@gmail.com>
2021-11-21 03:35:23 +0300

    Created hover effect for the text overlay to appear

--- js/scripts.js
+++ js/scripts.js
@@ -31,3 +31,60 @@ $("#product").click(function(){
         });
     });
 });
+$("#one").mouseenter(function(){
+    $("#jim").show(function(){
+        $(this). mouseleave(function(){
+            $(this).hide();
+        })
+    });
+})
+$("#two").mouseenter(function(){
+    $("#yellow").show(function(){
+        $(this). mouseleave(function(){
+            $(this).hide();
+        })
+    });
+})
+$("#three").mouseenter(function(){
+    $("#ontario").show(function(){
+        $(this). mouseleave(function(){
+            $(this).hide();
+        })
+    });
+})
+
+$("#four").mouseenter(function(){
+    $("#black").show(function(){
+        $(this). mouseleave(function(){
+            $(this).hide();
+        })
+    });
+})
+$("#five").mouseenter(function(){
+    $("#landing").show(function(){
+        $(this). mouseleave(function(){
+            $(this).hide();
+        })
+    });
+})
+$("#six").mouseenter(function(){
+    $("#calculate").show(function(){
+        $(this). mouseleave(function(){
+            $(this).hide();
+        })
+    });
+})
+$("#seven").mouseenter(function(){
+    $("#burned").show(function(){
+        $(this). mouseleave(function(){
+            $(this).hide();
+        })
+    });
+})
+$("#eight").mouseenter(function(){
+    $("#giraffe").show(function(){
+        $(this). mouseleave(function(){
+            $(this).hide();
+        })
+    });
+})
