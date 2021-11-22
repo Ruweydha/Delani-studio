4780f1d5362b493e00aa960cb03e2878131de40c
Ruweydha Abdinoor <ruweydhaabdinoor@gmail.com>
2021-11-22 09:21:10 +0300

    Called on those functions

--- js/scripts.js
+++ js/scripts.js
@@ -20,17 +20,7 @@ function validate(){
 
 }
 //  User interface logic
-// $("#design").click(function(){
-//     $(".designImg").hide( function(){
-//         $(".DesignWording").show( function(){
-//             $("#heading1").css('font-weight', 'bold');
-//             $(this).click(function(){
-//                 $(this).hide()
-//                 $(".designImg").show();
-//             });
-//         });
-//     });
-// });
+
 function alternate (id1, id2, id3,id4){
      $(id1).click(function(){
          $(id2).hide(function(){
@@ -48,28 +38,7 @@ alternate("#design",".designImg", ".DesignWording", "#heading1" );
 alternate("#development", ".developmentImg", ".DevelopmentWording", "#heading2");
 alternate("#product", ".productImg", ".ProductWording", "#heading3");
 
-// $("#development").click(function(){
-//     $(".developmentImg").hide( function(){
-//         $(".DevelopmentWording").show( function(){
-//             $("#heading2").css('font-weight', 'bold');
-//             $(this).click(function(){
-//                 $(this).hide()
-//                 $(".developmentImg").show();
-//             });
-//         });
-//     });
-// });
-// $("#product").click(function(){
-//     $(".productImg").hide( function(){
-//         $(".ProductWording").show( function(){
-//             $("#heading3").css('font-weight', 'bold');
-//             $(this).click(function(){
-//                 $(this).hide()
-//                 $(".productImg").show();
-//             });
-//         });
-//     });
-// });
+
 
 function hovering(id1, id2){
     $(id1).mouseenter(function(){
