f970a2ea3541160a12cc039a6288ff05ea364f32
Ruweydha Abdinoor <ruweydhaabdinoor@gmail.com>
2021-11-22 09:18:58 +0300

    Created a function for toggling between the images and text in the what we do section

--- js/scripts.js
+++ js/scripts.js
@@ -20,39 +20,56 @@ function validate(){
 
 }
 //  User interface logic
-$("#design").click(function(){
-    $(".designImg").hide( function(){
-        $(".DesignWording").show( function(){
-            $("#heading1").css('font-weight', 'bold');
-            $(this).click(function(){
-                $(this).hide()
-                $(".designImg").show();
-            });
-        });
-    });
-});
-$("#development").click(function(){
-    $(".developmentImg").hide( function(){
-        $(".DevelopmentWording").show( function(){
-            $("#heading2").css('font-weight', 'bold');
-            $(this).click(function(){
-                $(this).hide()
-                $(".developmentImg").show();
-            });
-        });
-    });
-});
-$("#product").click(function(){
-    $(".productImg").hide( function(){
-        $(".ProductWording").show( function(){
-            $("#heading3").css('font-weight', 'bold');
-            $(this).click(function(){
-                $(this).hide()
-                $(".productImg").show();
-            });
-        });
-    });
-});
+// $("#design").click(function(){
+//     $(".designImg").hide( function(){
+//         $(".DesignWording").show( function(){
+//             $("#heading1").css('font-weight', 'bold');
+//             $(this).click(function(){
+//                 $(this).hide()
+//                 $(".designImg").show();
+//             });
+//         });
+//     });
+// });
+function alternate (id1, id2, id3,id4){
+     $(id1).click(function(){
+         $(id2).hide(function(){
+             $(id3).show(function(){
+                 $(id4).css('font-weight', 'bold');
+                 $(id3).click(function(){
+                     $(id3).hide()
+                     $(id2).show();
+                 });
+             });
+         });
+     });
+}
+alternate("#design",".designImg", ".DesignWording", "#heading1" );
+alternate("#development", ".developmentImg", ".DevelopmentWording", "#heading2");
+alternate("#product", ".productImg", ".ProductWording", "#heading3");
+
+// $("#development").click(function(){
+//     $(".developmentImg").hide( function(){
+//         $(".DevelopmentWording").show( function(){
+//             $("#heading2").css('font-weight', 'bold');
+//             $(this).click(function(){
+//                 $(this).hide()
+//                 $(".developmentImg").show();
+//             });
+//         });
+//     });
+// });
+// $("#product").click(function(){
+//     $(".productImg").hide( function(){
+//         $(".ProductWording").show( function(){
+//             $("#heading3").css('font-weight', 'bold');
+//             $(this).click(function(){
+//                 $(this).hide()
+//                 $(".productImg").show();
+//             });
+//         });
+//     });
+// });
 
 function hovering(id1, id2){
     $(id1).mouseenter(function(){
